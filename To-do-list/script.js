839acf8ef3b8d067ee5b9906f08a4311c61896a5
Edgar Napoles <edgarnapolest@gmail.com>
2026-09-08 15:48:42 -0500

    agregando inputs y textareas para crear la informacion de las tareas

--- To-do-list/script.js
+++ To-do-list/script.js
@@ -1,4 +1,6 @@
 let tareas = []   // el array-estado, arranca vacío
+let descripcion = []
+let titulo = []
 
 function pintarLista() {
     let listaSidebar = document.querySelector(".sidebar__list-items")
@@ -8,22 +10,37 @@ function pintarLista() {
         let tarea = tareas[i]
         listaSidebar.innerHTML += `<li data-id="${tarea.id}">${tarea.texto}</li>`
     }
+    for (let i = 0; i < tareas.length; i++) {
+        let tarea = tareas[i]
+        listaSidebar.innerHTML += `<li data-id="${tarea.id}">${tarea.texto}</li>`
+    }
+    for (let i = 0; i < tareas.length; i++) {
+        let tarea = tareas[i]
+        listaSidebar.innerHTML += `<li data-id="${tarea.id}">${tarea.texto}</li>`
+    }
 }
 
 let formulario = document.querySelector(".sidebar__list-form")
 let inputNuevaTarea = document.querySelector(".sidebar__list-input")
+let inputNuevaTareaTitulo = document.querySelector(".main__list-title-input")
+let inputNuevaTareaDescripcion = document.querySelector(".main__list-description-textarea")
 
 formulario.addEventListener("submit", function (evento) {
-    evento.preventDefault()   // evita que el form recargue la página (comportamiento por defecto de submit)
-    
+
+    evento.preventDefault()
+
     let nuevaTarea = {
-        id: Date.now(),               // truco simple para un id "único": la marca de tiempo actual
+        id: Date.now(),              
         texto: inputNuevaTarea.value,
+        descripcion: inputNuevaTareaDescripcion.value,
+        titulo: inputNuevaTareaTitulo.value,
         completada: false
     }
-    
     tareas.push(nuevaTarea)
     pintarLista()
-    inputNuevaTarea.value = ""        // limpia el input después de agregar
+    inputNuevaTarea.value = ""   
+    inputNuevaTareaDescripcion.value = ""   
+    inputNuevaTareaTitulo.value = ""   
+    
 })
 
