df60d3fb60ae7915d05bef26786ead18d5020164
Edgar Napoles <edgarnapolest@gmail.com>
2026-09-08 16:37:51 -0500

    arreglando boton de agregar y selecionando el titulo como el nombre de tarea nueva

--- To-do-list/script.js
+++ To-do-list/script.js
@@ -1,46 +1,47 @@
-let tareas = []   // el array-estado, arranca vacío
-let descripcion = []
-let titulo = []
+let tareas = []
+
+let inputNuevaTarea = document.querySelector(".sidebar__list-input")
+let inputNuevaTareaTitulo = document.querySelector(".main__list-title-input")
+let inputNuevaTareaDescripcion = document.querySelector(".main__list-description-textarea")
+let botonAgregarNuevaTarea = document.querySelector('[data-valor="agregar"]')
+let botonBorrarTarea = document.querySelector('[data-valor = "borrar"]')
+
+//clases
+class Tareas{
+    constructor(id,titulo,descripcion,completada){
+        this.id = id
+        this.titulo = titulo
+        this.descripcion = descripcion
+        this.completada =  completada
+    }
+}
+
+//funciones de la sidebar
 
 function pintarLista() {
     let listaSidebar = document.querySelector(".sidebar__list-items")
     listaSidebar.innerHTML = ""
-    
     for (let i = 0; i < tareas.length; i++) {
         let tarea = tareas[i]
-        listaSidebar.innerHTML += `<li data-id="${tarea.id}">${tarea.texto}</li>`
-    }
-    for (let i = 0; i < tareas.length; i++) {
-        let tarea = tareas[i]
-        listaSidebar.innerHTML += `<li data-id="${tarea.id}">${tarea.texto}</li>`
-    }
-    for (let i = 0; i < tareas.length; i++) {
-        let tarea = tareas[i]
-        listaSidebar.innerHTML += `<li data-id="${tarea.id}">${tarea.texto}</li>`
+        listaSidebar.innerHTML += `<li data-id="${tarea.id}">${tarea.titulo}</li>`
     }
 }
 
-let formulario = document.querySelector(".sidebar__list-form")
-let inputNuevaTarea = document.querySelector(".sidebar__list-input")
-let inputNuevaTareaTitulo = document.querySelector(".main__list-title-input")
-let inputNuevaTareaDescripcion = document.querySelector(".main__list-description-textarea")
-
-formulario.addEventListener("submit", function (evento) {
-
+botonAgregarNuevaTarea.addEventListener("click", function (evento) {
     evento.preventDefault()
-
-    let nuevaTarea = {
-        id: Date.now(),              
-        texto: inputNuevaTarea.value,
-        descripcion: inputNuevaTareaDescripcion.value,
-        titulo: inputNuevaTareaTitulo.value,
-        completada: false
-    }
-    tareas.push(nuevaTarea)
+    
+    let tareaNueva = new Tareas(Date.now(),inputNuevaTareaTitulo.value,inputNuevaTareaDescripcion.value,false) 
+    tareas.push(tareaNueva)
     pintarLista()
     inputNuevaTarea.value = ""   
     inputNuevaTareaDescripcion.value = ""   
     inputNuevaTareaTitulo.value = ""   
-    
 })
+
+// botonBorrarTarea.addEventListener("click",function(){
+//     evento.preventDefault()
+//     let borradoDeTarea = {
+
+//     }
+// })
 
